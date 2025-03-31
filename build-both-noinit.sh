b5490661776c3653fe75cc7d0c8aa0091fdde9d8
Paul Williamson <paul@mustbeart.com>
2025-03-31 10:55:56 -0700

    Add noinit build scripts

---
+++ build-both-noinit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This script can run on a machine with the cross-development tools installed,
+# or it can run inside a container that provides the cross-development tools.
+# To build the example script inside the provided container, use:
+#     docker run --platform=linux/amd64 --volume .:/repo build-application /bin/sh build-both-loopback-rx.sh
+
+arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D NO_INIT_ON_SUCCESS -D RF_LOOPBACK -o msk_test-xmit msk_rx_init.c -lpthread -liio -lm -Wall -Wextra
+arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D NO_INIT_ON_SUCCESS -D RX_ACTIVE -o msk_test-recv msk_rx_init.c -lpthread -liio -lm -Wall -Wextra
