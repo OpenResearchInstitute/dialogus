bf6ad828e3d29bf793101298b054ba46154981af
Paul Williamson <paul@mustbeart.com>
2025-11-16 23:03:23 -0800

    Move binaries into bin

--- build-both-prbstest.sh
+++ build-both-prbstest.sh
@@ -4,5 +4,5 @@
 # To build the example script inside the provided container, use:
 #     docker run --platform=linux/amd64 --volume .:/repo build-application /bin/sh build-both-loopback-rx.sh
 
-arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D NO_INIT_ON_SUCCESS -D RF_LOOPBACK -o msk_prbstest-xmit opulent_voice.c -lpthread -liio -lm -Wall -Wextra
-arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D NO_INIT_ON_SUCCESS -D RX_ACTIVE -o msk_prbstest-recv opulent_voice.c -lpthread -liio -lm -Wall -Wextra
+arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D NO_INIT_ON_SUCCESS -D RF_LOOPBACK -o bin/msk_prbstest-xmit opulent_voice.c -lpthread -liio -lm -Wall -Wextra
+arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D NO_INIT_ON_SUCCESS -D RX_ACTIVE -o bin/msk_prbstest-recv opulent_voice.c -lpthread -liio -lm -Wall -Wextra
--- build-tx-opulent-voice.sh
+++ build-tx-opulent-voice.sh
@@ -4,5 +4,5 @@
 # To build the example script inside the provided container, use:
 #     docker run --platform=linux/amd64 --volume .:/repo build-application /bin/sh build-both-loopback-rx.sh
 
-arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D OVP_FRAME_MODE -o msk_opv-xmit opulent_voice.c -lpthread -liio -lm -Wall -Wextra -Wl,-z,stack-size=32768
-#arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D RX_ACTIVE -o msk_test-recv msk_rx_init.c -lpthread -liio -lm -Wall -Wextra
+arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D OVP_FRAME_MODE -o bin/msk_opv-xmit opulent_voice.c -lpthread -liio -lm -Wall -Wextra -Wl,-z,stack-size=32768
+#arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=$HOME/pluto-0.38.sysroot -g -D ENDLESS_PRBS -D RX_ACTIVE -o bin/msk_test-recv msk_rx_init.c -lpthread -liio -lm -Wall -Wextra
