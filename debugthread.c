de015c1566b692ddcf78b583b824a6fdd7cd16c4
Paul Williamson <paul@mustbeart.com>
2025-12-01 00:12:53 -0800

    Rx FIFO debug to match test firmware

--- debugthread.c
+++ debugthread.c
@@ -25,7 +25,9 @@ void* ovp_debug_thread_func(__attribute__((unused)) void *arg) {
 	bool frame_buffer_overflow __attribute__((unused));
 	uint32_t frames_received __attribute__((unused)) = 0;
 	uint32_t frame_sync_errors __attribute__((unused)) = 0;
-	char *state_names[8] = { "IDLE", "COLLECT", "ENC_START", "ENC_RUN", "ENC_FLUSH", "INTERLEAVE", "OUTPUT", "iNvALiD" };
+	char *tx_state_names[8] = { "IDLE", "COLLECT", "ENC_START", "ENC_RUN", "ENC_FLUSH", "INTERLEAVE", "OUTPUT", "iNvALiD" };
+	char *rx_state_names[8] = { "IDLE", "COLLECT", "EXTRACT", "DEINTERLEAVE", "PREP_FEC_DECODE", "DERANDOMIZE", "InVaLiD" };
+
 
 	while (!stop) {
 		uint32_t now;
@@ -58,23 +60,30 @@ void* ovp_debug_thread_func(__attribute__((unused)) void *arg) {
 			// VRVR QLxx xwww wwww wwws ssrr rrrr rrrr
 
 //			printf("DEBUG: %02x %s wr=%03x rd=%03x\n", (tx_fifo_reg & 0xfc000000) >> 26,
-//													   state_names[(tx_fifo_reg & 0x03800000) >> 23],
+//													   tx_state_names[(tx_fifo_reg & 0x03800000) >> 23],
 //													   (tx_fifo_reg & 0x007FE000) >> 13,
 //													   (tx_fifo_reg & 0x000003FF)
 //						);
-			printf("DEBUG: etv=%x etr=%x ftv=%x ftr=%x txr=%x etl=%x %s wr=%03x rd=%03x rxwr=%03x rxrd=%03x\n", 
+			printf("DEBUG TX: etv=%x etr=%x ftv=%x ftr=%x txr=%x etl=%x %s wr=%03x rd=%03x\n", 
 						(tx_fifo_reg & 0x80000000) >> 31,
 						(tx_fifo_reg & 0x40000000) >> 30,
 						(tx_fifo_reg & 0x20000000) >> 29,
 						(tx_fifo_reg & 0x10000000) >> 28,
 						(tx_fifo_reg & 0x08000000) >> 27,
 						(tx_fifo_reg & 0x04000000) >> 26,
-						state_names[(tx_fifo_reg & 0x03800000) >> 23],
+						tx_state_names[(tx_fifo_reg & 0x03800000) >> 23],
 						(tx_fifo_reg & 0x007FE000) >> 13,
-						(tx_fifo_reg & 0x000003FF),
-
-						(rx_fifo_reg & 0x03FF0000) >> 16,
-						(rx_fifo_reg & 0x000003FF)
+						(tx_fifo_reg & 0x000003FF)
+						);
+			printf("DEBUG RX: vstart=%x vbusy=%x vdone=%x dectv=%x dectr=%x %s wr=%03x rd=%03x\n",
+						(rx_fifo_reg & 0x10000000) >> 28,
+						(rx_fifo_reg & 0x08000000) >> 27,
+						(rx_fifo_reg & 0x04000000) >> 26,
+						(rx_fifo_reg & 0x02000000) >> 25,
+						(rx_fifo_reg & 0x01000000) >> 24,
+						rx_state_names[(rx_fifo_reg & 0xe0000000) >> 29],
+						(rx_fifo_reg & 0x007FE000) >> 13,
+						(rx_fifo_reg & 0x000003FF)						
 						);
 			printf("debugthread power at %d %d ", now,
 							capture_and_read_msk(OFFSET_MSK(rx_power)));
