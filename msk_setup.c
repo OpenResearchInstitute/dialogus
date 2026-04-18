7b526569ce4b4638969c64b69736a4179db32c49
Paul Williamson <paul@mustbeart.com>
2026-04-18 16:55:17 -0700

    P-gain search baseline

--- msk_setup.c
+++ msk_setup.c
@@ -138,7 +138,7 @@ void msk_setup(void)
 	printf("LPF_Config_2: (0x%08x@%04x)\n", READ_MSK(LPF_Config_2), OFFSET_MSK(LPF_Config_2));
 
 	printf("Write a default value for symbol lock count and threshold.\n");
-	WRITE_MSK(symbol_lock_control, 0x002e2010);	// symbol lock count = 0x10 (16), symbol lock threshold = 0x0bb8 (3000)
+	WRITE_MSK(symbol_lock_control, 0x002e2010);	// symbol lock count = 0x10 (16), symbol lock threshold = 0x0b88 (2952)
 	printf("symbol_lock_control: (0x%08x@%04x)\n", READ_MSK(symbol_lock_control), OFFSET_MSK(symbol_lock_control));
 
 	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
@@ -166,8 +166,10 @@ void msk_setup(void)
 */
 
 	int32_t proportional_gain =           0x007FFFFF;	// 0x00000243; //0x0012984F for 32 bits 0x00001298 for 24 bits 243 for OE 
-	int32_t integral_gain =          	  0x007FFFFF;	// 0x000005A7; //0x0000C067 for 32 bits and 80 for 0E
-	int32_t proportional_gain_bit_shift = 20;	// Proportional gain: 0x14 shift (20)
+//	int32_t integral_gain =          	  0x007FFFFF;	// 0x000005A7; //0x0000C067 for 32 bits and 80 for 0E
+	int32_t integral_gain =          	  0x00000000;	// zero integral for searching proportional gain
+//	int32_t proportional_gain_bit_shift = 20;	// Proportional gain: 0x14 shift (20)
+	int32_t proportional_gain_bit_shift = 16;	// Proportional gain: start at 0x10 shift (16)
 	int32_t integral_gain_bit_shift =     29;	// Integral gain: 0x1d shift (29)
 
 	int32_t proportional_config = (proportional_gain_bit_shift << 24) | (proportional_gain & 0x00FFFFFF);
